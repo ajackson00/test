d59c9c319cff69b0f3c5adf939acb0892bbf7762
ajackson00 <angelicajackson@glsllc.com>
2014-07-24 15:25:00 -0400

    Verifications

---
+++ Verifications/Funding Rules/Index_files/ControlValues.js
@@ -0,0 +1,69 @@
+// JScript source code
+function CompareArrays(arrayA, arrayB) {
+    if (!(arrayA instanceof Array && arrayB instanceof Array)) { return false; }
+    if (arrayA.length != arrayB.length) { return false; }
+
+    var a = jQuery.extend(true, [], arrayA);
+    var b = jQuery.extend(true, [], arrayB);
+    for (var i = 0, l = a.length; i < l; i++) {
+        if (a[i] !== b[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function GetControlValues() {
+    return GetControlValuesBasedonClassName('.formClass');
+}
+
+function GetControlValuesBasedonClassName(className) {
+    var objDictionary = new Array();
+
+    for (i = 0; i < $(className).length; i++) {
+        try {
+            var element = $(className)[i];
+            objDictionary[i] = GetControlValue(element);
+        } catch(ex) {
+            objDictionary[i] = "";
+        }
+        
+    }
+    return objDictionary;
+}
+
+function GetControlValue(element) {
+    if (element.type == 'checkbox') {
+        return element.checked;
+    }
+    else if (element.type == 'radio') {
+        return $('input:radio[name=' + element.name + ']:checked').val();
+    }
+    else if (element.type == 'select-one') {
+        return $(element).filter(':selected').val() || $(element).val();
+    }
+    else if (element.type == 'select-multiple') {
+        var options = [];
+        $.each($(element).find('option'), function() { options.push(this.value); });
+        var selectedValues = $(element).filter(':selected').val() || $(element).val() || [];
+        return options.concat(selectedValues).join(', ');
+    }
+    else if (element.type == 'textarea' || element.localName == 'input') {
+        return element.value;
+    }
+
+    return element.innerHTML;
+}
+
+function FormControlState(rootElement, filter) {
+    var objDictionary = new Array();
+    var inputs = $(rootElement).find(filter);
+    _.each(inputs, function(input, i) {
+        try {
+            objDictionary[i] = GetControlValue(input);
+        } catch (ex) {
+            objDictionary[i] = "";
+        }
+    });
+    return objDictionary;
+}
---
+++ Verifications/Upload Center/Upload Center Config_files/adminUploadCenter.js
@@ -0,0 +1,51 @@
+﻿
+var viewModel;
+    
+$(function () {
+
+    $.ajax({
+        type: 'GET',
+        url: "/admin/uploadcenter/getConfig",
+        contentType: 'application/json; charset=utf-8',
+        success: function (data) {
+            viewModel = ko.mapping.fromJS(data);
+            ko.applyBindings(viewModel);
+        },
+        error: function (data) {
+            notyModal("An error occurred getting current settings.");
+        }
+    });
+});
+
+
+function save() {
+    //required for config wizard
+
+    var postData = ko.mapping.toJS(viewModel);
+
+    $.ajax({
+        type: 'POST',
+        url: "/admin/uploadcenter/save",
+        contentType: 'application/json; charset=utf-8',
+        data: JSON.stringify({ Model: postData }),
+        success: function(data) {
+            notyModal("Saved Successfully");
+        },
+        error: function(data) {
+            notyModal("An error occurred trying to save.");
+        }
+    });
+}
+    
+function cancelClick() {
+    var newValues = GetControlValues();
+    var compareValues = CompareArrays(newValues, oldValues);
+    if (!compareValues) {
+        if (confirm("Clicking ok will cancel your changes!")) {
+            $.noty.closeAll();
+        }
+    }
+    else {
+        $.noty.closeAll();
+    }
+}
